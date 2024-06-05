f6c4e004b9e7af1f9e6083978c0167385dc19a98
Tim Rowe <tim@tjsr.id.au>
2024-06-06 09:19:02 +1000

    Fixes for testing db container.

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import { DEFAULT_HTTP_PORT, startApp } from './src/server.js';
 import { intEnv, loadEnv, requireEnv } from '@tjsr/simple-env-utils';
 
 import express from 'express';
+import { mysqlSessionStore } from '@tjsr/user-session-middleware';
+
+console.log(`Starting with NODE_ENV ${process.env['NODE_ENV']}`);
 
 loadEnv();
 
@@ -11,7 +14,7 @@ requireEnv('HTTP_PORT');
 
 const HTTP_PORT: number = intEnv('HTTP_PORT', DEFAULT_HTTP_PORT);
 
-const app: express.Express = startApp();
+const app: express.Express = startApp(mysqlSessionStore);
 app.listen(HTTP_PORT, () => {
   console.log(`Listening on port ${HTTP_PORT}`);
 });
